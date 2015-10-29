83026f311fa40b812d051c5c4e570a92d34bdacc
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-10-29 22:52:45 +0100

    Shitty ip address checking

--- app/src/main/java/com/ktulu/ktulu/MainActivity.java
+++ app/src/main/java/com/ktulu/ktulu/MainActivity.java
@@ -1,15 +1,80 @@
 package com.ktulu.ktulu;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView ip;
+
+    private class CheckMyIp extends AsyncTask<Void, Void, Void> {
+        private String ipNumber;
+        private WifiInfo wifiInfo;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d("Ktulu", "LEL");
+            ipNumber = "";
+            WifiManager mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            wifiInfo = mainWifi.getConnectionInfo();
+            byte[] bytes = BigInteger.valueOf(wifiInfo.getIpAddress()).toByteArray();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            List<Byte> byteList = new ArrayList<Byte>();
+            for(byte b : bytes) {
+                byteList.add(b);
+            }
+
+            Collections.reverse(byteList);
+
+            for(byte b : byteList) {
+                int x = (b + 256) % 256;
+                stringBuilder.append(String.valueOf(x));
+                stringBuilder.append(".");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+
+            ipNumber = stringBuilder.toString();
+
+            Log.d("Ktulu", ipNumber);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            ip.setText("IP Number: " + ipNumber);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ip = (TextView)findViewById(R.id.ip);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +93,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        new CheckMyIp().execute();
     }
 
     @Override
