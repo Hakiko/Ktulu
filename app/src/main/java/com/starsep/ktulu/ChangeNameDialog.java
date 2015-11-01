9795722eaf5500fa69b10d178e8e41a49b294571
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-11-02 00:11:09 +0100

    Name and ChangeNameDialog

---
+++ app/src/main/java/com/starsep/ktulu/ChangeNameDialog.java
@@ -0,0 +1,41 @@
+package com.starsep.ktulu;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ChangeNameDialog extends DialogFragment {
+    private String oldName;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final EditText editText = new EditText(getContext());
+        editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        editText.setText(oldName);
+
+        builder.setMessage(getString(R.string.changeNameTitle))
+                .setView(editText)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ((MainActivity)getActivity()).updateNameUi(editText.getText().toString());
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null);
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    public void setOldName(String name) {
+        oldName = name;
+    }
+}
--- app/src/main/java/com/starsep/ktulu/MainActivity.java
+++ app/src/main/java/com/starsep/ktulu/MainActivity.java
@@ -2,24 +2,27 @@ package com.starsep.ktulu;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView ip;
+    private TextView ipTextView;
+    private TextView nameTextView;
+    private SharedPreferences settings;
 
     private class CheckMyIp extends AsyncTask<Void, Void, Void> {
         private String myIpNumber;
@@ -73,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                     .append("\nTethering: ")
                     .append(tetheringOn ? "On" : "Off");
 
-            ip.setText(stringBuilder.toString());
+            ipTextView.setText(stringBuilder.toString());
         }
     }
 
@@ -82,16 +85,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ip = (TextView) findViewById(R.id.ip);
+        ipTextView = (TextView) findViewById(R.id.ip);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         Button refreshButton = (Button) findViewById(R.id.refreshButton);
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,6 +112,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        TextView version = (TextView) findViewById(R.id.version);
+        version.setText("Ktulu v" + BuildConfig.VERSION_NAME);
+
+        nameTextView = (TextView) findViewById(R.id.name);
+        updateNameUi(null);
+    }
+
+    public void updateNameUi(String name) {
+        if(name != null) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("name", name);
+            editor.apply();
+            //Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+        }
+        nameTextView.setText(settings.getString("name", "Default Value"));
     }
 
     private void startServerService() {
@@ -127,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        settings = getPreferences(MODE_PRIVATE);
         makeUi();
         new CheckMyIp().execute();
         startServerService();
@@ -147,7 +159,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_change_name) {
+            //Toast.makeText(getApplicationContext(), "Change name clicked", Toast.LENGTH_LONG).show();
+            ChangeNameDialog dialog = new ChangeNameDialog();
+            dialog.setOldName(settings.getString("name", getString(R.string.defaultName)));
+            dialog.show(getSupportFragmentManager(), "ChangeNameDialog");
             return true;
         }
 
